e11101d4000e17ac7044f6c4d24e42d2a4aa831f
ikramebakkari <56300696+ikramebakkari@users.noreply.github.com>
2020-05-09 01:01:07 +0200

    Update txnscript.java

--- txnscript.java
+++ txnscript.java
@@ -339,6 +339,28 @@ public class txnscript
 		return result ;
     }
 	
+	// creation fonction pour EXB1613
+    public static String updateVilleByName (String nom, Integer codePostal)
+    {
+        String result = "" ;
+		String sql = "UPDATE Villes SET code_postal = ? WHERE nom = ?" ;
+
+		try
+		{
+				PreparedStatement pstmt = cnx.prepareStatement(sql) ;
+				pstmt.setDouble(1, codePostal);
+				pstmt.setString(2, nom);
+				pstmt.executeUpdate();
+		}
+		catch (SQLException e)
+		{
+			System.out.println(e.getMessage());
+		}
+		
+		return result ;
+    }
+
+	
 
 	
 	
